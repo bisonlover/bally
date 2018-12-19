1929989ff8fc17f015283df94492df4f3ed325b7
Bisonlover <aidan2008@bellsouth.net>
2018-12-19 17:38:31 -0500

    commit

---
+++ Assets/camera.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camera : MonoBehaviour {
+    public GameObject sphere;
+    public float speedH = 2.0f;
+    private float yaw = 0.0f;
+    private Vector3 offset;
+    // Use this for initialization
+    void Start () {
+        offset = transform.position - sphere.transform.position;
+    }
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        yaw += speedH * Input.GetAxis("Mouse x");
+        transform.eulerAngles = new Vector3(0.0f, yaw, 0.0f);
+        sphere.GetComponent<script_player>().yaw = yaw;
+        transform.position = sphere.transform.position + offset;
+    }
+}
---
+++ Assets/script_player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script_player : MonoBehaviour {
+    public float speed;
+    public float speedH = 2.0f;
+
+    public GameObject bally;
+
+    private Rigidbody rb;
+    public float yaw = 0.0f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        
+
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        movement = Quaternion.Euler(0, yaw, 0) * movement;
+
+        rb.AddForce(movement * speed);
+
+        rb.AddTorque(transform.up * moveHorizontal *100);
+        rb.AddTorque(transform.right * moveVertical);
+    }
+}
